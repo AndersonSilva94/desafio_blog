a36f2c276772d91386ace748626521d6fb26d505
Anderson Silva <andssilva.23@gmail.com>
2022-11-16 22:17:23 -0400

    [A] feat: :rocket: criando uma página NotFound

--- blog_angular/src/app/app-routing.module.ts
+++ blog_angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/pages/home/home.component';
+import { NotFoundComponent } from 'src/app/pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'news-details',
     loadChildren: () =>  import('./pages/news-details/news-details.module').then(m => m.NewsDetailsModule),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- blog_angular/src/app/app.module.ts
+++ blog_angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
