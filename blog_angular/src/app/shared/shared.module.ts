ae44c352a5ee87fc842022eb838eba50f116f812
Anderson Silva <andssilva.23@gmail.com>
2022-11-18 01:49:05 -0400

    [A] feat: :rocket: criando o componente InputSearch

--- blog_angular/src/app/shared/shared.module.ts
+++ blog_angular/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    InputSearchComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    InputSearchComponent
   ]
 })
 export class SharedModule { }
