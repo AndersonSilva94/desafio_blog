7b42bd374d9f1ee1b616a58afa7c6c4d015ef0a0
Anderson Silva <andssilva.23@gmail.com>
2022-11-18 00:57:39 -0400

    [A] feat: :rocket: criando o componente Header

--- blog_angular/src/app/shared/shared.module.ts
+++ blog_angular/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class SharedModule { }
