8e831e7c97e0bf1b5b21129c88501cf3e45229f9
Anderson Silva <andssilva.23@gmail.com>
2022-11-19 03:32:22 -0400

    [A] feat: :rocket: criando o template de card de notícias

--- blog_angular/src/app/shared/components/card-news/card-news.component.ts
+++ blog_angular/src/app/shared/components/card-news/card-news.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardNewsComponent implements OnInit {
 
+  mockCard = {
+    publicationDate: "2021-07-12T03:25:06.000Z",
+    title: "Agora é oficial: o Windows 11 está vindo",
+    sinopse: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.",
+  }
+
   constructor() { }
 
   ngOnInit(): void {
