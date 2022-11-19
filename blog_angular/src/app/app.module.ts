3669f5bbe9c7838fe23ad945456ae902fb6c9d5b
Anderson Silva <andssilva.23@gmail.com>
2022-11-19 03:30:35 -0400

    [A] feat: :rocket: estruturando o locale para o pipe de data

--- blog_angular/src/app/app.module.ts
+++ blog_angular/src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 import { NotFoundComponent } from 'src/app/pages/not-found/not-found.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
