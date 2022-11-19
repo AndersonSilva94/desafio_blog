4df92be5a484d06edde2868c2fcdefbc38f12480
Anderson Silva <andssilva.23@gmail.com>
2022-11-19 02:17:52 -0400

    [A] feat: :rocket: criando o componente de card news

---
+++ blog_angular/src/app/shared/components/card-news/card-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-news',
+  templateUrl: './card-news.component.html',
+  styleUrls: ['./card-news.component.scss']
+})
+export class CardNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- blog_angular/src/app/shared/shared.module.ts
+++ blog_angular/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { InputSearchComponent } from './components/input-search/input-search.component';
+import { CardNewsComponent } from './components/card-news/card-news.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    InputSearchComponent
+    InputSearchComponent,
+    CardNewsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    InputSearchComponent
+    InputSearchComponent,
+    CardNewsComponent
   ]
 })
 export class SharedModule { }
